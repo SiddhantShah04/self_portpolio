0e0a42c2cf142bf99804aba5d8237b9c016a3c36
siddhantshah04 <siddhantshah04@gmail.com>
2023-01-22 14:00:04 +0530

    image loader

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
-module.exports = {
-    images: {
-      loader: 'akamai',
-      path: '/',
+const withPWA = require('next-pwa')
+
+module.exports = withPWA({
+    pwa: {
+        disable: process.env.NODE_ENV === 'development',
+        dest: 'public'
     },
-  }
+    images: {
+        domains: ['media-exp1.licdn.com', 'www.nonceblox.com'],
+        loader: 'akamai',
+        path: '/',
+    }
+})
