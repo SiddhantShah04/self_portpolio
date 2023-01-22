84cf34e14d81391dc990af23cec7ffa4b54ec993
siddhantshah04 <siddhantshah04@gmail.com>
2023-01-22 13:59:18 +0530

    images fix

--- next.config.js
+++ next.config.js
@@ -1,12 +1,6 @@
-const withPWA = require('next-pwa')
-
-module.exports = withPWA({
-    pwa: {
-        disable: process.env.NODE_ENV === 'development',
-        dest: 'public'
-    },
+module.exports = {
     images: {
-        domains: ['media-exp1.licdn.com', 'www.nonceblox.com'],
-        unoptimized: true,
-    }
-})
+      loader: 'akamai',
+      path: '/',
+    },
+  }
